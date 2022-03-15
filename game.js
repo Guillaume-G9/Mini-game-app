fc21c51826e0e9b010e49d05a4824ab674ae117a
Guillaume-G9 <ggonthier@live.fr>
2022-03-16 00:06:28 +0100

    still need to fix some bugs

--- game.js
+++ game.js
@@ -1,38 +1,47 @@
 class Character{
-    constructor(name, health, tired) {
-        this.name = name;
-        this.health = health || 100;
-        this.tired = tired || false;
-        this.stamina = 35;
-        this.isAlive = true;
-        this.canAttack = true
-
+    constructor(name, health, stamina) {
+        this.name = name
+        this.health = health || 100
+        this.tired = false
+        this.stamina =  stamina || 20
+        this.isAlive = true
     }
     attack(playerAttacked) {
+        
         this.stamina -= 5
-        playerAttacked.health -= Math.floor(Math.random() * 50) + 20;
+        playerAttacked.health -= Math.floor(Math.random() * 50) + 20
+        console.log(`Il reste ${playerAttacked.health} points de vie à ${playerAttacked.name}`)
+        console.log(`${this.name} a ${this.stamina} points d'endurance`)
+
         if (this.stamina < 1) {
-            this.tired = true;
-            console.log(`${this.name} ne peut plus attaquer !`)
+            this.tired = true
         }
         if (playerAttacked.health < 1) {
             playerAttacked.isAlive = false
             console.log(`${playerAttacked.name} est mourru !`)
+            return
         }
     }
 }
 
 function mortalKombat(player1, player2) {
-
-    do {
-      player1.attack(player2)
-      player2.attack(player1)
-    } while (player1.health > 1 || player2.health > 1)
- 
-    if (player1.health < 1 && player2.health > 1) {
-        console.log(`${player1.name} a gagné !`)
-    } else if (player2.health < 1 && player1.health > 1) {
-        console.log(`${player2.name} a gagné !`)
+    for (tour = 0; tour == player1.isAlive === false || player2.isAlive === false; tour++) {
+        console.log(`Tour ${tour} :`)
+        if (this.tired === true) {
+            console.log(`${this.name} ne peut plus attaquer !`)
+            return
+        } else if (tour % 2 === 1) {
+            player1.attack(player2)
+        } else {
+            player2.attack(player1)
+        }
+    }
+    if (player1.health < 1 || player2.health < 1) {
+        if (player1.isAlive === true && player2.isAlive === false) {
+            console.log(`${player1.name} a gagné !`)
+        } else if (player2.isAlive === true && player1.isAlive === false) {
+            console.log(`${player2.name} a gagné !`)
+        }
     }
 }
     
--- game.test.js
+++ game.test.js
@@ -3,36 +3,49 @@ let {mortalKombat} = require('./game')
 
 
 
-test('should have a name', () => {
-    let perso1 = new Character('Brousse_Lit')
-    let perso2 = new Character('Jicévédé')
+// test('should have a name', () => {
+//     let perso1 = new Character('Brousse_Lit')
+//     let perso2 = new Character('Jicévédé')
 
-    expect(perso1.name).toBe('Brousse_Lit')
-    expect(perso2.name).toBe('Jicévédé')
-})
+//     expect(perso1.name).toBe('Brousse_Lit')
+//     expect(perso2.name).toBe('Jicévédé')
+// })
 
-test('should have different health capacity', ()=> {
-    let perso1 = new Character('Brousse_Lit', 145)
-    let perso2 = new Character('Jicévédé')
+// test('should have different health capacity', () => {
+//     let perso1 = new Character('Brousse_Lit', 145)
+//     let perso2 = new Character('Jicévédé')
 
-    expect(perso1.health).toBe(145)
-    expect(perso2.health).toBe(100)
-})
+//     expect(perso1.health).toBe(145)
+//     expect(perso2.health).toBe(100)
+// })
 
-test('one player should die when health is below 0', () => {
-    let perso1 = new Character('Brousse_Lit', 145)
-    let perso2 = new Character('Jicévédé', 15)
+// test('should consume stamina when attacking', () => {
+//     let perso1 = new Character('Brousse_Lit', 300, 15)
+//     let perso2 = new Character('Jicévédé', 300)
 
-    expect(perso2.isAlive).toBe(true)
-    perso1.attack(perso2)
-    expect(perso2.isAlive).toBe(false)
-})
+//     perso1.attack(perso2)
+//     perso1.attack(perso2)
+//     perso1.attack(perso2)
+
+//     expect(perso1.tired).toBe(true)
+    
+// })
+
+
+// test('one player should die when health is below 0', () => {
+//     let perso1 = new Character('Brousse_Lit', 145)
+//     let perso2 = new Character('Jicévédé', 15)
+
+//     expect(perso2.isAlive).toBe(true)
+//     perso1.attack(perso2)
+//     expect(perso2.isAlive).toBe(false)
+// })
 
 test('two players should fight together', () => {
-    let perso1 = new Character('Brousse_Lit', 10)
-    let perso2 = new Character('Jicévédé', 200)
+    let perso1 = new Character('Brousse_Lit', 10, 10)
+    let perso2 = new Character('Jicévédé', 200, 10)
 
     mortalKombat(perso1,perso2)
-    expect(console.log)
+    expect()
     
 })
